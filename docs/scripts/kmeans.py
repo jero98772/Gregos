53805a78d097a55a5bf2957882be03d61d2b66b1
dasjero98772 <jero98772@gmail.com>
2023-04-12 14:13:32 -0500

    udpate kmeans

--- docs/scripts/kmeans.py
+++ docs/scripts/kmeans.py
@@ -1,21 +1,33 @@
 from pyspark.sql import SparkSession
 from pyspark.ml.linalg import Vector
 from pyspark.ml.feature import VectorAssembler
-
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.evaluation import ClusteringEvaluator 
+import pyspark.sql.functions as f
 
 spark=SparkSession.builder.getOrCreate()
 df=spark.read.csv("chess_openings.csv",inferSchema=True,header=True)
 df.show()
 df.count()
 df.createOrReplaceTempView("chess_opening")
-spark.sql("select count(opening_name) from chess_opening").show()
+spark.sql("select opening_name from chess_opening").show()
 spark.sql("select count( DISTINCT opening_name) from chess_opening").show()
-#1477
-df.printSchema()
-df.describe()
 
+data= df.select("moves").rdd.flatMap(lambda x: x).collect()
+print(data)
+k=1477
+kmeans=KMeans(k=k).fit(data)
+#model=kmeans.fit(df)
+#model.transform("e4 e5 d4 d4").groupBy("prediction").count().show()
+print(model)
+
+"""df.printSchema()
+df.describe()
+le = SparkLabelEncoder()
+le.fit(y)
 vec_assembler=VectorAssembler(inputCols="moves",outputCol="opening_name")
 vec_assembler.transform(df)
+"""
 #a=set(df["opening_name"])
 #print(a)
 #print(len(a))
