91c4de18adfaff0c69d9b8467370b793ed75d682
jero98772 <jero98772@protonmail.com>
2023-04-20 13:04:27 -0500

    update

--- docs/scripts/cleardataset.py
+++ docs/scripts/cleardataset.py
@@ -6,6 +6,27 @@ board = chess.Board()
 l="abcdefgh"
 n="12345678"
 mytable = str.maketrans(l, n)
+
+def separate_moves(X,col_name="moves"):
+	newdf=pd.DataFrame({"opening_name":df["opening_name"]})
+	#for i in X.shape[0]:
+
+	#n_moves = X.shape[0]#break every game in individual moves
+	#moves = [[] for x in range(n_moves-1)]
+	for _ in range(n_moves-1):
+		game = eval(X[col_name][_])
+	for move in game:
+		try:
+			player_move = move.split(" ")
+		#print(player_move)
+			moves[_].append(player_move)
+			#moves[_].append(player_move[1]) #add white move
+			#moves[_].append(player_move[2]) #add black move
+		except:
+      #if error occurs
+			print(_, move)
+	print(moves)
+	#return moves
 def changeNotation(san_mainline):
 	san_mainline=san_mainline.split()
 	#ns=""
@@ -23,13 +44,40 @@ def changeNotation(san_mainline):
 	#print(san_mainline)
 	return tmp
 
+def test(df):
+	a=df["moves"].apply(lambda x:len(x))
+	#print(a.max(),a.idxmax())
+	#print(df.iloc[a.idxmax()])
+	n_cols = 2#a.max()
+
+	# calculate the number of rows in each column
+	n_rows = a.max()
+
+	# reshape the data
+	reshaped_data = df.to_numpy().reshape((-1, n_rows)).T
+
+	# create a list of column names
+	col_names = [f"col_{i+1}" for i in range(n_cols)]
+
+	# create a new dataframe using the reshaped data and column names
+	new_df = pd.DataFrame(data=reshaped_data, columns=col_names)
+	print(new_df)
 #print(uci2algebraic(board,"d4"))
-df=pd.read_csv("chess_openings.csv")#.head(5)
+df=pd.read_csv("chess_openings2moves.csv")#.head(5)
+
+test(df)
+#a=pd.DataFrame((df["moves"]))
+#a=df["moves"].apply(lambda x:len(x))
+#print(a.max(),a.idxmax())
+#print(df.iloc[a.idxmax()])
+#print(separate_moves(df,"moves"))
 #df.iloc[5]
-df["moves"]=df["moves"].apply(changeNotation)
-print(df)
-newdf=pd.DataFrame({"opening_name":df["opening_name"],"moves":df["moves"]})
+#df["moves"]=df["moves"].apply(changeNotation)
+#print(df)
+#newdf=pd.DataFrame({"opening_name":df["opening_name"],"moves":df["moves"]})
 #newdf.to_csv("chess_openings3movestransalted.csv")
+
+
 
 #l="abcdefgh"
 #n="12345678"
