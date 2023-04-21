93154be730bca9f3a33db0af76f30967b4213971
jero98772 <jero98772@protonmail.com>
2023-04-21 11:04:37 -0500

    clear new dataset, only integers

--- docs/scripts/cleardf.py
+++ docs/scripts/cleardf.py
@@ -8,7 +8,7 @@ def adapt_data(df,colname="moves"):
 	cols,_=getMaxCols(df)
 	print(cols,_)
 	matrix=[]
-	cols_names=["opening_name"]+["move_"+str(i) for i in range(cols)]
+	cols_names=["opening_name"]+[i for i in range(cols)]
 	#matrix=np.empty([rows,cols],dtype="S10")
 	for i in range(rows):
 		moves=eval(df[colname][i])
@@ -20,10 +20,10 @@ def adapt_data(df,colname="moves"):
 			#matrix[i][j]=moves[j]
 	return matrix,cols_names
 def main():
-	df=pd.read_csv("chess_openings2moves.csv")#.head(10)
+	df=pd.read_csv("chess_openings3movestransalted.csv")#.head(10)
 	data,cols=adapt_data(df,colname="moves")
 	ndf = pd.DataFrame(data, columns =cols);print(ndf) 
-	ndf.to_csv("chess_openings4individualcolumns.csv")
+	ndf.to_csv("chess_openings5individualcolumnsintegers.csv")
 	#print(adapt_data(df,colname="moves"))
 	#print(eval(df["moves"][0]),eval(df["moves"][0])[11])
 main()
